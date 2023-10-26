9545153380da00fa6637e10f72ae1a821d54bedb
Maciej Rajtar <rajmac93@gmail.com>
2023-10-26 19:58:22 +0200

    some changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
+const closeHamburger = document.querySelector(".hamburger--close");
+const sideBar = document.querySelector(".sidebar");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", function () {
   console.log("asd");
+  sideBar.style.display = "flex";
+});
+closeHamburger.addEventListener("click", function () {
+  console.log("asd");
+  sideBar.style.display = "none";
 });
