05e1af524e6a4624526956ec8afdcd1805af8cb1
Maciej Rajtar <rajmac93@gmail.com>
2023-10-28 23:27:33 +0200

    fixed navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
 const hamburger = document.querySelector(".hamburger");
 const closeHamburger = document.querySelector(".hamburger--close");
 const sideBar = document.querySelector(".sidebar");
+const mobileNavCloseSidebar = document.querySelectorAll(".nav__close__sidebar");
 
 hamburger.addEventListener("click", function () {
-  console.log("asd");
   sideBar.style.display = "flex";
 });
+
 closeHamburger.addEventListener("click", function () {
-  console.log("asd");
   sideBar.style.display = "none";
+});
+
+mobileNavCloseSidebar.forEach((el) => {
+  el.addEventListener("click", function () {
+    sideBar.style.display = "none";
+  });
+  console.log(el);
 });
