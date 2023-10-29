d61043b13cf9714475e929f62b5ef41155139e69
Maciej Rajtar <rajmac93@gmail.com>
2023-10-29 13:14:00 +0100

    first attempt with modal

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const closeHamburger = document.querySelector(".hamburger--close");
 const sideBar = document.querySelector(".sidebar");
 const mobileNavCloseSidebar = document.querySelectorAll(".nav__close__sidebar");
+const contactBtn = document.querySelector(".contact__btn");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const pageProgressBar = document.querySelector(".progress-bar");
+const modalBtnClose = document.querySelector(".modal__btn--close");
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back--to--top");
 
 hamburger.addEventListener("click", function () {
   sideBar.style.display = "flex";
@@ -16,10 +23,9 @@ mobileNavCloseSidebar.forEach((el) => {
     sideBar.style.display = "none";
   });
 });
-
-const showOnPx = 100;
-const backToTopButton = document.querySelector(".back--to--top");
-
+// SIDEBAR EVENTS
+//________________________________________________________________
+// SCROLL TO TOP EVENTS
 const scrollContainer = () => {
   return document.documentElement || document.body;
 };
@@ -31,14 +37,13 @@ document.addEventListener("scroll", () => {
     backToTopButton.classList.add("back--to--top__hidden");
   }
 });
+
 const goToTop = () => {
   document.body.scrollIntoView({
     behavior: "smooth",
   });
 };
 backToTopButton.addEventListener("click", goToTop);
-
-const pageProgressBar = document.querySelector(".progress-bar");
 document.addEventListener("scroll", () => {
   const scrolledPercentage =
     (scrollContainer().scrollTop /
@@ -53,3 +58,26 @@ document.addEventListener("scroll", () => {
     backToTopButton.classList.add("hidden");
   }
 });
+// SCROLL TO TOP EVENTS
+//________________________________________________________________
+// MODAL
+const openModal = () => {
+  modal.classList.remove("modal__hidden");
+  overlay.classList.remove("overlay__hidden");
+};
+const closeModal = () => {
+  modal.classList.add("modal__hidden");
+  overlay.classList.add("overlay__hidden");
+};
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === `Escape`) {
+    modal.classList.add("modal__hidden");
+    overlay.classList.add("overlay__hidden");
+  }
+});
+
+contactBtn.addEventListener("click", openModal);
+modalBtnClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+// MODAL
